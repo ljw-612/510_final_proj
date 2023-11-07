3b6a2d8db76830eb7314cf7790a998519b230016
tommymmcguire <tmm126@duke.edu>
2023-11-07 11:20:52 -0500

    stuff

--- web_scrape_JL.py
+++ web_scrape_JL.py
@@ -26,29 +26,42 @@ index = 3
 
 with open(csv_filename, "w", newline="", encoding="utf-8") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(
-        [
-            "index",
-            "title", 
-            "group"]
-        )
+    writer.writerow(["index", "title", "group"])
     while True:
-        
+
         try:
-            xpath = "/html/body/div[5]/div[2]/div/div/div[6]/div[1]/ul/li[{}]/div/div/div[2]/div/div/h3".format(index)
+            xpath = "/html/body/div[5]/div[2]/div/div/div[6]/div[1]/ul/li[{}]/div/div/div[2]/div/div/h3".format(
+                index
+            )
             title = (
                 WebDriverWait(driver, 2)
-                .until(EC.presence_of_element_located((By.XPATH, xpath,)))
+                .until(
+                    EC.presence_of_element_located(
+                        (
+                            By.XPATH,
+                            xpath,
+                        )
+                    )
+                )
                 .text
             )
         except:
             title = ""
 
         try:
-            xpath = "/html/body/div[5]/div[2]/div/div/div[6]/div[1]/ul/li[{}]/div/div/div[2]/div/div/div[2]".format(index)
+            xpath = "/html/body/div[5]/div[2]/div/div/div[6]/div[1]/ul/li[{}]/div/div/div[2]/div/div/div[2]".format(
+                index
+            )
             group = (
                 WebDriverWait(driver, 2)
-                .until(EC.presence_of_element_located((By.XPATH, xpath,)))
+                .until(
+                    EC.presence_of_element_located(
+                        (
+                            By.XPATH,
+                            xpath,
+                        )
+                    )
+                )
                 .text
             )
         except:
@@ -59,10 +72,3 @@ with open(csv_filename, "w", newline="", encoding="utf-8") as csvfile:
         if title and group:
             writer = csv.writer(csvfile)
             writer.writerow([title, group])
-
-
-
-
-
-
-
