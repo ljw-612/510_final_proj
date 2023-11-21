faf3427f0c0519c18a4391b1573e9c51d8bf777e
Jingwei Li <lijingwei58466809@outlook.com>
2023-11-21 10:41:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,18 +35,19 @@ def search_notebook(df, search_term, n=3, pprint=True, client=None):
     # search_terms = [Sports, Spiritual or Religious, Health, Wellness, Women, Food, Career, Climate, Education, Black, Community]
     # search_terms_len = [214, 65, 139, 137, 7, 117, 180, 11, 155, 12, 424]
 
-    threshold = 0.78
+    # threshold = 0.78
     df['ada_embedding'] = df['ada_embedding'].apply(eval).apply(np.array)
 
+    # get the embedding of the search term
     model = "text-embedding-ada-002"
     search_embedding = client.embeddings.create(input=search_term, model=model).data[0].embedding
     search_embedding = np.array(search_embedding).reshape(1, -1)
 
+    # calculate the cosine similarity between the search term and the entries in the database
     df['similarity'] = df['ada_embedding'].apply(lambda x: cosine_similarity(x.reshape(1,-1), search_embedding))
+    # df_thres = df[df['similarity'] > threshold]
 
-    df_thres = df[df['similarity'] > threshold]
-
-
+    # get the top n results
     result = (
         df.sort_values(by='similarity', ascending=False)
         .head(n)
@@ -65,6 +66,7 @@ def main():
                                 placeholder = "Paste your OpenAI API key here",
                                 type = "password")
 
+    # load the OpenAI API key
     if user_secret:
         openai.api_key = user_secret
         client = OpenAI(api_key=user_secret)
@@ -88,7 +90,7 @@ def main():
 
             for index, row in answer.iterrows():
                 st.write(row['similarity'][0][0])
-
+                # indicate the type of the resource using different colors
                 if row['Type'] == 'Events':
                     st.write(f" :blue[{row['Type']}:]", row['Name'])
                 elif row['Type'] == 'Group':
